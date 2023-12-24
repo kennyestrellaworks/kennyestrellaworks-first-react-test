d2f507ae439c951b8dad501666ab0a50473d3bdf
kennyestrellaworks <kennyestrellaworks@gmail.com>
2023-12-24 15:56:35 +0800

    Cleaning some errors in this React practice.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import PersonContent from './PersonContent';
+import PersonContent from "./PersonContent";
 
-import './sass/_base.scss';
-import './sass/_colors.scss';
-import './sass/_components.scss';
-import './sass/_fonts.scss';
-import './sass/_layout.scss';
-import './sass/_variables.scss';
-
-function App() {
+export default function App() {
   return (
     <>
-    <div className="wrap">
-      <div className="wrap__screen">
-        <div className="wrap__viewport">
-          <div className="wrap__content">
-            <div className="person">
-              <div className="person__heading">
-                  <h1 className="heading-style-1 heading-style-1__primary">employee list</h1>
-                  <h4 className="heading-style-1 heading-style-1__secondary">People From Team Webby</h4>
+      <div className="wrap">
+        <div className="wrap__screen">
+          <div className="wrap__viewport">
+            <div className="wrap__content">
+              <div className="person">
+                <div className="person__heading">
+                  <h1 className="heading-style-1 heading-style-1__primary">
+                    employee list
+                  </h1>
+                  <h4 className="heading-style-1 heading-style-1__secondary">
+                    People From Team Webby
+                  </h4>
+                </div>
+                <section className="person__content">
+                  <PersonContent />
+                </section>
               </div>
-              <section className="person__content">              
-                <PersonContent />
-              </section>  
-            </div>            
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
-
-export default App;
---
+++ src/PersonContent.js
@@ -0,0 +1,38 @@
+import data from "./data.js";
+
+export default function PersonContent() {
+  const personDetails = data.person_details;
+  const person = personDetails.map((person) => {
+    const { id, first_name, last_name, job_position } = person.profile_info;
+    const fullName = first_name + " " + last_name;
+    const imageURL = "images/" + id + "-img.jpg";
+    return (
+      <div className="person__item">
+        <div className="person__item-wrap">
+          <>
+            <div className="profile-card">
+              <div className="profile-card__profile">
+                <img
+                  src={imageURL}
+                  alt={fullName}
+                  className="profile-card__profile-picture"
+                />
+                <h1 className="heading-style-2 heading-style-2__primary profile-card__person-name">
+                  {fullName}
+                </h1>
+                <h2 className="heading-style-2 heading-style-2__secondary profile-card__person-position">
+                  {job_position}
+                </h2>
+                <div
+                  className="profile-card__profile-social"
+                  data-id={id}
+                ></div>
+              </div>
+            </div>
+          </>
+        </div>
+      </div>
+    );
+  });
+  return person;
+}
